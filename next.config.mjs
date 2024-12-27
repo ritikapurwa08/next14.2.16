35cccb3c39410abda07791f4830d0d3acd4cd3da
ritik-apurwa08 <8ballpoolrk2@gmail.com>
2024-12-27 11:27:50 +0530

    this is the main branch commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
